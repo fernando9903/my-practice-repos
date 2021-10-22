9e78d33fbfb981a0cbc9a654d1e68f12141898ba
jose fernando <fernando.parra9903@gmail.com>
2021-10-22 09:32:12 -0700

    file system created

--- freecodecamp/nodeforbeginners/app.js
+++ freecodecamp/nodeforbeginners/app.js
@@ -27,18 +27,60 @@
 // })
 // fernando.emit('name');
 
-const readline = require('readline');
-const rl = readline.createInterface({
-    input : process.stdin,
-    output: process.stdout,
-});
+// const readline = require('readline');
+// const rl = readline.createInterface({
+//     input : process.stdin,
+//     output: process.stdout,
+// });
 
-let num1 = Math.floor((Math.random() * 10) + 1);
-let num2 = Math.floor((Math.random() * 10) + 1);
+// let num1 = Math.floor((Math.random() * 10) + 1);
+// let num2 = Math.floor((Math.random() * 10) + 1);
 
-let answer = num1 + num2;
+// let answer = num1 + num2;
 
-rl.question(`What is ${num1} + ${num2} ? `, (userInput) => {
-    console.log(userInput);
-})
+// rl.question(`What is ${num1} + ${num2} ? `, (userInput) => {
+//     console.log(userInput);
+// })
 
+const fs = require('fs');
+//create a file
+// fs.writeFile('example.txt', "This is an example", (err) => {
+//     if(err){
+//         console.log(err);
+//     } else {
+//         console.log('file successfuly created!')
+//         fs.readFile('example.txt', 'utf-8',(err, file) => {
+//             if(err) {
+//                 console.log('error leyendo');
+//             } else {
+//                 console.log(file);
+//             }
+//         });
+//     }
+// });
+
+
+//Rename the file
+// fs.rename('example.txt', 'exampleRename.txt', (err) => {
+//     if(err){
+//         console.log(err);
+//     } else {
+//         console.log('renombrado!')
+//     }
+// })
+
+// fs.appendFile('exampleRename.txt', 'some data beging appended', (err) => {
+//     if(err){
+//         console.log(err)
+//     } else {
+//         console.log('Successfully appended data to the file')
+//     }
+// })
+
+// fs.unlink('exampleRename.txt', (err) => {
+//     if(err){
+//         console.log(err);
+//     } else {
+//         console.log("Eliminado correctamente!")
+//     }
+// })
