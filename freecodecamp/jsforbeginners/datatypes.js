1c0bb251ea70be11722173e4fbcc4c920b82a1fc
jose fernando <fernando.parra9903@gmail.com>
2021-10-29 17:26:18 -0700

    jsforbeginners: dataTypes

---
+++ freecodecamp/jsforbeginners/datatypes.js
@@ -0,0 +1,14 @@
+/* Data Types:
+    undefined, null, boolean, string, symbol, bumber, and object
+*/
+
+//this var can be access in the hole project
+var myName = 'Fernando';
+
+myName =  8;
+
+//This var only can be access into the scope where was declare
+let computersName = 'Dell';
+
+//I valu that never change
+const pi = Math.PI;
