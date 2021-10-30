600f20f5b2458fd518c0aa9a10f25ef39addcad3
jose fernando <fernando.parra9903@gmail.com>
2021-10-30 14:12:15 -0700

    gifexpert:categories added

--- fernando-herrera-courses/react-de-cero-a-experto/04-gif-expert-app/src/GifExpertApp.js
+++ fernando-herrera-courses/react-de-cero-a-experto/04-gif-expert-app/src/GifExpertApp.js
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(
+        [
+            'Sword Art Online',
+            'Asoka Tano',
+            'Star Wars',
+            'Your lie in April',
+        ]
+    )
+
+    const handleAddCategory = (event) => {
+        setCategories(
+            [
+                ...categories,
+                'Something new category'
+            ]
+        )
+    }
+
     return (
         <>
             <h2>GifExpertApp</h2>
             <hr />
+            <button onClick={handleAddCategory}>Add Category</button>
+            <ol>
+                {
+                    categories.map( (category) =>
+                        <li key={category}>{category}</li>
+                    )
+                }
+            </ol>
         </>
     );
 }
